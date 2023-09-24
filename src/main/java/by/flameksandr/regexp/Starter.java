4d242d8be6ef1908ded01c2aecd84dae49a68349
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 16:59:01 +0300

    ? - символ либо есть либо нету + - 1 и более повторений * - 0 или есть повторения

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 public class Starter {
     public static void main(String[] args) {
 
-        System.out.println(Pattern.compile("10{3,}").matcher("1000000 abra cadabra").find());
-        System.out.println(Pattern.compile("10{1,}").matcher("000000 abra cadabra").find());
-        System.out.println(Pattern.compile("10{1,}").matcher("1 abra cadabra").find());
+        System.out.println(Pattern.compile("10?").matcher("1000000 abra cadabra").find());
+        System.out.println(Pattern.compile("10*").matcher("000000 abra cadabra").find());
+        System.out.println(Pattern.compile("10+").matcher("1 abra cadabra").find());
 
 
     }
