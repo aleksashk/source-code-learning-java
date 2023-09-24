45448614a4e3f6dd550f6212205d324befc06284
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 16:44:48 +0300

    ?= то поиск в строке будет осуществляться наперед и совпадения не будут запоминаться

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -6,11 +6,11 @@ import java.util.regex.Pattern;
 public class Starter {
     public static void main(String[] args) {
 
-        Pattern pattern = Pattern.compile("(:?Mouse|Keyboard)Listener");
-        Matcher matcher = pattern.matcher("MouseListener KeyboardListener");
+        Pattern pattern = Pattern.compile("Java (?=7|8)");
+        Matcher matcher = pattern.matcher("Java 7 Java 8");
 
         while (matcher.find()){
-            System.out.println(matcher.group(1));
+            System.out.println(matcher.group());
         }
 
     }
