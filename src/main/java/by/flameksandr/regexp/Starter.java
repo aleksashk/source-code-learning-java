6a1863d4ac163fb5844326cb090f54c90d65cf19
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 17:05:09 +0300

    ленивая квантификация - захватит все совпадения, которые стоят перед квантификатором .*? -

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -1,14 +1,25 @@
 package by.flameksandr.regexp;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Starter {
     public static void main(String[] args) {
 
-        System.out.println(Pattern.compile("10?").matcher("1000000 abra cadabra").find());
-        System.out.println(Pattern.compile("10*").matcher("000000 abra cadabra").find());
-        System.out.println(Pattern.compile("10+").matcher("1 abra cadabra").find());
+        int counter = 0;
+        String string = "196.198.1.197";
+        Pattern pattern = Pattern.compile(".*?19");
+        Matcher matcher = pattern.matcher(string);
 
+        while (matcher.find()) {
+            System.out.println("Match found '" +
+                    string.substring(matcher.start(), matcher.end()) +
+                    "'. Starting at index " +
+                    " and ending at index " + matcher.end());
+            counter++;
+        }
+
+        System.out.println("Matchers found: " + counter);
 
     }
 }
