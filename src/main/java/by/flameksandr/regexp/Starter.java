b5063d80c9e8974ffc2c45f5b8d1d48506d2cc1f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 16:55:39 +0300

    true - если строка содержит от 1 до 3 0

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -6,11 +6,10 @@ import java.util.regex.Pattern;
 public class Starter {
     public static void main(String[] args) {
 
-        System.out.println(Pattern.compile("10{2}").matcher("100 abra cadabra").find());
-        //*********************** альтернативный вариант
-        Pattern pattern = Pattern.compile("10{2}");
-        Matcher matcher = pattern.matcher("100 abra cadabra.");
-        System.out.println(matcher.find());
+        System.out.println(Pattern.compile("10{1,3}").matcher("1000000 abra cadabra").find());
+        System.out.println(Pattern.compile("10{1,3}").matcher("000000 abra cadabra").find());
+        System.out.println(Pattern.compile("10{1,3}").matcher("1 abra cadabra").find());
+
 
 
     }
