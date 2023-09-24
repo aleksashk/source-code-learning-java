640fe8ab87884e7470d52ec97027e43b5211fbef
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 16:46:46 +0300

    ?! то поиск несоответсвия в строке будет осуществляться наперед и совпадения не будут запоминаться

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -6,17 +6,11 @@ import java.util.regex.Pattern;
 public class Starter {
     public static void main(String[] args) {
 
-        Pattern pattern = Pattern.compile("Java (?=7|8)");
-        Matcher matcher = pattern.matcher("Java 7 Java 8");
-        Matcher matcher1 = pattern.matcher("Java 9");
+        Pattern pattern = Pattern.compile("Java (?!7|8)");
+        Matcher matcher = pattern.matcher("Java 7 Java 8 Java 9");
 
         while (matcher.find()){
             System.out.println(matcher.group());
         }
-
-        while (matcher1.find()){
-            System.out.println(matcher.group());
-        }
-
     }
 }
