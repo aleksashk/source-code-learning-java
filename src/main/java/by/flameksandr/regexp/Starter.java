13d2fb2669a9fa0d72651ff32d22a09928fbbd1f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 16:42:10 +0300

    если группа используется только для группировки и результат нас не интересует, по перед шаблоном добавляется ?: в данном случае совпадение не запоминается

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class Starter {
     public static void main(String[] args) {
 
-        Pattern pattern = Pattern.compile("(\\d+).*\\1");
-        Matcher matcher = pattern.matcher("2016 year. 2017 year. 2018 year");
+        Pattern pattern = Pattern.compile("(:?Mouse|Keyboard)Listener");
+        Matcher matcher = pattern.matcher("MouseListener KeyboardListener");
 
         while (matcher.find()){
             System.out.println(matcher.group(1));
