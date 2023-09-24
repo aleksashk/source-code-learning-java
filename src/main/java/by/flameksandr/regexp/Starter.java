dd9deb4876007a5349d41af741bc984374084a49
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 18:48:20 +0300

    Проверка данных на валидность номер кредитной карты, срок ее действия и cvv

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -6,13 +6,22 @@ import java.util.regex.Pattern;
 public class Starter {
     public static void main(String[] args) {
 
-        String phoneNumber = "3750174568712";
-        Pattern pattern = Pattern.compile("^((\\+?375)(\\p{Digit}{10}))$");
-        Matcher matcher = pattern.matcher(phoneNumber);
-        if(matcher.find()){
-            System.out.println("PhoneNumber '" + phoneNumber + "' is correct!");
-        }else {
-            System.out.println("PhoneNumber '" + phoneNumber + "' is incorrect!");
+        String cardNumber = "1234 5678 9658 4521";
+        String date = "12/25";
+        String cvv = "569";
+
+        Pattern cardNumberPattern = Pattern.compile("([2-6]([0-9]{3}) ?)(([0-9]{4} ?){3})");
+        Pattern datePattern = Pattern.compile("(0[1-9]|1[0-2])/([0-9]{2})");
+        Pattern cvvPattern = Pattern.compile("[0-9]{3}");
+        Matcher cardNumberMatcher = cardNumberPattern.matcher(cardNumber);
+        Matcher dateMatcher = datePattern.matcher(date);
+        Matcher cvvMatcher = cvvPattern.matcher(cvv);
+        if (cardNumberMatcher.find() &&
+                dateMatcher.find() &&
+                cvvMatcher.find()) {
+            System.out.println("PhoneNumber '" + cardNumber + " " + date + " " + cvv + "' is correct!");
+        } else {
+            System.out.println("PhoneNumber '" + cardNumber + " " + date + " " + cvv + "' is incorrect!");
         }
     }
 }
