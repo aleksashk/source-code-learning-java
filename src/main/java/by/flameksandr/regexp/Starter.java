4b043f5962eb0ab2e51d6937edd500eda0c38336
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 16:54:03 +0300

    true - так как 0 повторяется 2 раза refactoring

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -1,11 +1,17 @@
 package by.flameksandr.regexp;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Starter {
     public static void main(String[] args) {
 
         System.out.println(Pattern.compile("10{2}").matcher("100 abra cadabra").find());
+        //*********************** альтернативный вариант
+        Pattern pattern = Pattern.compile("10{2}");
+        Matcher matcher = pattern.matcher("100 abra cadabra.");
+        System.out.println(matcher.find());
+
 
     }
 }
