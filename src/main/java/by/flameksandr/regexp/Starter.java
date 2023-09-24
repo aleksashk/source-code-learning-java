47c6234160510a3fd6e4ed3e6a55535da24af88f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 16:52:10 +0300

    true - так как 0 повторяется 2 раза

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Starter {
     public static void main(String[] args) {
 
-        System.out.println(Pattern.compile("10{2}").matcher("10").find());
+        System.out.println(Pattern.compile("10{2}").matcher("100 abra cadabra").find());
 
     }
 }
