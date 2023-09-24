305a0d19a5b6af0aad99c4b8b116c6b2919eb32a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-24 16:51:04 +0300

    квантификаторы позволяют искать символы или группы символов, которые повторяются определенное количество раз - количество повторов определяется типами квантификаторов. Квантификатор применяется к символу/группе символов перед ним. false - так как 0 повторяется 1 раз, а в квантификаторе 2

--- src/main/java/by/flameksandr/regexp/Starter.java
+++ src/main/java/by/flameksandr/regexp/Starter.java
@@ -1,16 +1,11 @@
 package by.flameksandr.regexp;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Starter {
     public static void main(String[] args) {
 
-        Pattern pattern = Pattern.compile("Java (?!7|8)");
-        Matcher matcher = pattern.matcher("Java 7 Java 8 Java 9");
+        System.out.println(Pattern.compile("10{2}").matcher("10").find());
 
-        while (matcher.find()){
-            System.out.println(matcher.group());
-        }
     }
 }
