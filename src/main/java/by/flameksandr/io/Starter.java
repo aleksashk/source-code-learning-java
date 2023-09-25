68711925d3a1bc496fb2d2a9c31ccb4fa30ca0a9
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 13:38:43 +0300

    Запись в файл через символьный поток

--- src/main/java/by/flameksandr/io/Starter.java
+++ src/main/java/by/flameksandr/io/Starter.java
@@ -1,18 +1,21 @@
 package by.flameksandr.io;
 
-import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Starter {
     public static void main(String[] args) {
 
-        try (FileInputStream stream = new FileInputStream("text2.txt")) {
-            int symbol;
-            while ((symbol = stream.read()) != -1) {
-                System.out.print((char) symbol);
+        String string = "Hello world";
+        char[] chars = new char[string.length()];
+        string.getChars(0, chars.length, chars, 0);
+
+        try (FileWriter writer = new FileWriter("file10.txt")) {
+            for (char eachChars : chars) {
+                writer.write(eachChars);
             }
         } catch (IOException e) {
-            System.out.println("Input error");
+            System.out.println("Output error");
         }
     }
 }
