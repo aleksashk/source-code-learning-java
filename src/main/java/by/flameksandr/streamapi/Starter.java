a0e35c9c5d384dc1936be0cc68a999971b959751
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 15:57:34 +0300

    массив как источник данных

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -1,17 +1,16 @@
 package by.flameksandr.streamapi;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Starter {
     public static void main(String[] args) {
-        int[] numbers = {-1, 0, 3, -23, 12, -5};
-        for (int eachNumber : numbers) {
-            if (eachNumber < 0) {
-                System.out.println(eachNumber);
-            }
-        }
-        System.out.println("-------------------------------------------------------");
 
-        Arrays.stream(numbers).filter(value -> value < 0).forEach(System.out::println);
+        Stream<String> objectStream = Arrays.stream(new String[]{
+                "a", "b", "c"
+        });
+
+        objectStream.forEach(System.out::println);
+
     }
 }
