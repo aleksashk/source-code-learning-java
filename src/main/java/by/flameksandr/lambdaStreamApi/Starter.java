5c046f3af364b39c7361172e6ddf3e16634a798a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 14:32:53 +0300

    lambda with variables

--- src/main/java/by/flameksandr/lambdaStreamApi/Starter.java
+++ src/main/java/by/flameksandr/lambdaStreamApi/Starter.java
@@ -2,12 +2,17 @@ package by.flameksandr.lambdaStreamApi;
 
 public class Starter {
 
-    private static double value1 = 3;
-    private static double value2 = 5;
 
     public static void main(String[] args) {
 
-        Operation operation = () -> value1 + value2;
+
+        double value1 = 3;
+        double value2 = 5;
+
+        Operation operation = () -> {
+//            value1 = 100;
+            return value1 + value2;
+        };
         System.out.println(operation.getResult());
         System.out.println(value1);
         System.out.println(value2);
