20148a8d77c4257bafeae279164ae1700dc00da9
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 18:12:14 +0300

    collect()  V1

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -1,14 +1,14 @@
 package by.flameksandr.streamapi;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Starter {
     public static void main(String[] args) {
 
-        Stream<Integer> numbers = Stream.of();
-        Optional<Integer> result = numbers.min(Integer::compare);
-
-        System.out.println(result.orElseThrow(RuntimeException::new));
+        Stream<String> values = Stream.of("val1", "val2", "val3", "val4", "val5");
+        List<String> list = values.collect(Collectors.toList());
+        System.out.println(list);
     }
 }
