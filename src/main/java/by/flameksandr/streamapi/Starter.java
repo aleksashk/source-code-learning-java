7428be23d83424f8944c7dbf36927c56ba3de42a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 16:39:13 +0300

    Optional

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -1,18 +1,13 @@
 package by.flameksandr.streamapi;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Starter {
     public static void main(String[] args) {
 
-        Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5);
-        int result = numbers.reduce(0, (value1, value2) -> {
-            if (value2 < 5) {
-                return value1 + value2;
-            } else {
-                return value1;
-            }
-        }, (value1, value2) -> value1 + value2);
-        System.out.println(result);
+        Stream<Integer> numbers = Stream.of();
+        Optional<Integer> result = numbers.min(Integer::compare);
+        result.ifPresent(System.out::println);
     }
 }
