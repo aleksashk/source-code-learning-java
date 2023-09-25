cf229c088df553e0df07889b9dd2bde50f1d71ca
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 16:06:29 +0300

    filter

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -1,21 +1,14 @@
 package by.flameksandr.streamapi;
 
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Starter {
     public static void main(String[] args) {
 
-        Stream<String> objectStream = Stream.of("asd", "qwe", "zxcv");
-        objectStream.forEach(System.out::println);
+        ArrayList<String> names = new ArrayList<>();
+        Collections.addAll(names, "Franck", "Paolo", "Xenona");
 
-        IntStream intStream = IntStream.of(1, 2, 3, 4);
-        intStream.forEach(System.out::println);
-        LongStream longStream = LongStream.of(1L, 2L, 3L, 4L);
-        longStream.forEach(System.out::println);
-        DoubleStream doubleStream = DoubleStream.of(1D, 2D, 3D, 4D);
-        doubleStream.forEach(System.out::println);
+        names.stream().filter(name -> name.length() >= 6).forEach(System.out::println);
     }
 }
