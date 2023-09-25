eb3fe5a669711da72d212ba04874c12bba4a1c53
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 16:20:34 +0300

    преобразование элементов flatMap

---
+++ src/main/java/by/flameksandr/streamapi/Pizza.java
@@ -0,0 +1,11 @@
+package by.flameksandr.streamapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Pizza {
+    private String title;
+    private int price;
+}
--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -5,8 +5,10 @@ import java.util.stream.Stream;
 public class Starter {
     public static void main(String[] args) {
 
-        Stream<Film> filmStream = Stream.of(new Film("DeadPool", 100.0), new Film("Carbiners", 200.0), new Film("Mortal Combat", 150.0));
-        filmStream.map(Film::getPrice).forEach(System.out::println);
-
+        Stream<Pizza> pizzaStream = Stream.of(new Pizza("Peperoni", 90));
+        pizzaStream.flatMap(pizza -> Stream.of(
+                String.format("Pizza: %s, price: $%d", pizza.getTitle(), pizza.getPrice()),
+                String.format("HAPPY HOURS DISCOUNT -50%%)%nPizza: %s, price: $%d", pizza.getTitle(), pizza.getPrice() / 2)
+        )).forEach(System.out::println);
     }
 }
