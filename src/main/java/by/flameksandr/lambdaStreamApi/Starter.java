e59002ee27bb22b6b3162d2be53fec52a1fa6a44
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 14:48:18 +0300

    Обобщенные функциональные интерфейсы:Function, Predicate, Consumer, Supplier, UnaryOperator, BinaryOperator Supplier.get

--- src/main/java/by/flameksandr/lambdaStreamApi/Starter.java
+++ src/main/java/by/flameksandr/lambdaStreamApi/Starter.java
@@ -1,13 +1,20 @@
 package by.flameksandr.lambdaStreamApi;
 
-import java.util.function.Function;
+import java.util.Scanner;
+import java.util.function.Supplier;
 
 public class Starter {
 
 
     public static void main(String[] args) {
 
-        Function<Integer, Double> converter = value -> Double.valueOf(value);
-        System.out.println(converter.apply(345));
+        Supplier<String> text = () -> {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter text: ");
+            return scanner.nextLine();
+        };
+
+            System.out.println(text.get());
+
     }
 }
