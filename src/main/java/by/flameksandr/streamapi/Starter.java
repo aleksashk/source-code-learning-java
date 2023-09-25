b27ac7d95a9555abfadd76c34eb90c152634be0b
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 16:26:00 +0300

    сведение элементов noneMatch

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -6,10 +6,10 @@ public class Starter {
     public static void main(String[] args) {
         Stream<Integer> mixedNumbers = Stream.of(-2, -1, 0, 1, 2);
 
-        System.out.println(mixedNumbers.allMatch(number -> number > 0));
+        System.out.println(mixedNumbers.noneMatch(number -> number > 0));
         Stream<Integer> positiveNumbers = Stream.of(2, 1);
-        System.out.println(positiveNumbers.allMatch(number -> number > 0));
+        System.out.println(positiveNumbers.noneMatch(number -> number > 0));
         Stream<Integer> negativeNumbers = Stream.of(-2, -1);
-        System.out.println(negativeNumbers.allMatch(number -> number < 0));
+        System.out.println(negativeNumbers.noneMatch(number -> number < 0));
     }
 }
