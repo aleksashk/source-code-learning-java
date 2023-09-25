2c3f500c7e35b0d91d6e7183e6e639331a801a85
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 16:09:47 +0300

    фильтрация - промежуточная операция для потока данных

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -1,14 +1,11 @@
 package by.flameksandr.streamapi;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.stream.Stream;
 
 public class Starter {
     public static void main(String[] args) {
 
-        ArrayList<String> names = new ArrayList<>();
-        Collections.addAll(names, "Franck", "Paolo", "Xenona");
-
-        names.stream().filter(name -> name.length() >= 6).forEach(System.out::println);
+        Stream<String> passwords = Stream.of("asdf2342", "s5k3kf", "d7rhb", "xyz");
+        passwords.filter(password -> password.length() >= 5).forEach(System.out::println);
     }
 }
