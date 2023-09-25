e825ab595bf93adcf647e084b75d0ae8fceabbdd
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 16:03:39 +0300

    статический метод of

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -1,6 +1,5 @@
 package by.flameksandr.streamapi;
 
-import java.util.Arrays;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
@@ -9,19 +8,14 @@ import java.util.stream.Stream;
 public class Starter {
     public static void main(String[] args) {
 
-        Stream<String> objectStream = Arrays.stream(new String[]{
-                "a", "b", "c"
-        });
-
-        IntStream intStream = Arrays.stream(new int[]{1,2,3});
-        LongStream longStream = Arrays.stream(new long[]{1l,2l,3l});
-        DoubleStream doubleStream = Arrays.stream(new double[]{1d,2d,3d});
-
+        Stream<String> objectStream = Stream.of("asd", "qwe", "zxcv");
         objectStream.forEach(System.out::println);
 
+        IntStream intStream = IntStream.of(1, 2, 3, 4);
         intStream.forEach(System.out::println);
+        LongStream longStream = LongStream.of(1L, 2L, 3L, 4L);
         longStream.forEach(System.out::println);
+        DoubleStream doubleStream = DoubleStream.of(1D, 2D, 3D, 4D);
         doubleStream.forEach(System.out::println);
-
     }
 }
