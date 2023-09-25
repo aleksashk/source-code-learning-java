ad1ada590940a517a302711e17cb882494c315af
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 14:27:21 +0300

    lambda with block operators

---
+++ src/main/java/by/flameksandr/lambdaStreamApi/Factorial.java
@@ -0,0 +1,6 @@
+package by.flameksandr.lambdaStreamApi;
+
+@FunctionalInterface
+public interface Factorial {
+    int getResult(int value);
+}
--- src/main/java/by/flameksandr/lambdaStreamApi/Starter.java
+++ src/main/java/by/flameksandr/lambdaStreamApi/Starter.java
@@ -3,8 +3,14 @@ package by.flameksandr.lambdaStreamApi;
 public class Starter {
     public static void main(String[] args) {
 
-        Printer printer = string -> System.out.println(string);
-        printer.println("Hello world");
+        Factorial factorial = value -> {
+            int result = 1;
+            for (int i = 1; i < value; i++) {
+                result *= i;
+            }
+            return result;
+        };
 
+        System.out.println(factorial.getResult(4));
     }
 }
