499b37e0af1474641220329b1beadbd879deaa5d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 16:36:57 +0300

    сведение элементов reduce V3

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -6,7 +6,13 @@ public class Starter {
     public static void main(String[] args) {
 
         Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5);
-        int result = numbers.reduce(10, (value1, value2) -> value1 + value2);
+        int result = numbers.reduce(0, (value1, value2) -> {
+            if (value2 < 5) {
+                return value1 + value2;
+            } else {
+                return value1;
+            }
+        }, (value1, value2) -> value1 + value2);
         System.out.println(result);
     }
 }
