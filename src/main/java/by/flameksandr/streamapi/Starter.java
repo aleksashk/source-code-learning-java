874012d36f227746990595187f1ba3f82405c455
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 15:59:51 +0300

    массив как источник данных refactoring

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -1,6 +1,9 @@
 package by.flameksandr.streamapi;
 
 import java.util.Arrays;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Starter {
@@ -10,7 +13,15 @@ public class Starter {
                 "a", "b", "c"
         });
 
+        IntStream intStream = Arrays.stream(new int[]{1,2,3});
+        LongStream longStream = Arrays.stream(new long[]{1l,2l,3l});
+        DoubleStream doubleStream = Arrays.stream(new double[]{1d,2d,3d});
+
         objectStream.forEach(System.out::println);
+
+        intStream.forEach(System.out::println);
+        longStream.forEach(System.out::println);
+        doubleStream.forEach(System.out::println);
 
     }
 }
