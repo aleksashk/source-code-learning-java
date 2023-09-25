2190575c85da7b6b24ee27e428718066f36217cb
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 18:12:54 +0300

    collect()  V2

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -8,7 +8,6 @@ public class Starter {
     public static void main(String[] args) {
 
         Stream<String> values = Stream.of("val1", "val2", "val3", "val4", "val5");
-        List<String> list = values.collect(Collectors.toList());
-        System.out.println(list);
+        values.collect(Collectors.toSet()).forEach(System.out::println);
     }
 }
