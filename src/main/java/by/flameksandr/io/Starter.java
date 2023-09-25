ed7de1ead2a0ed4662043e7c71d8ccff53808b72
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 13:47:34 +0300

    Серриализация объекта - способность сохранять его копию и других объектов на которые он может ссылаться.

--- src/main/java/by/flameksandr/io/Starter.java
+++ src/main/java/by/flameksandr/io/Starter.java
@@ -1,18 +1,35 @@
 package by.flameksandr.io;
 
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 public class Starter {
-    public static void main(String[] args) {
 
-        try (FileReader reader = new FileReader("file10.txt")) {
-            int symbol;
-            while ((symbol = reader.read()) != -1) {
-                System.out.print((char)symbol);
-            }
+    private static final String PATH = "object.txt";
+
+    public static void main(String[] args) {
+        serialize();
+    }
+
+    private static void serialize() {
+        SerializableClass serializableClass = new SerializableClass(SerializableClass.class.getName(), 0);
+        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(PATH))) {
+            stream.writeObject(serializableClass);
         } catch (IOException e) {
-            System.out.println("Input error");
+            System.out.println("Output error");
+        }
+
+    }
+
+    private static class SerializableClass implements Serializable {
+        private String title;
+        private int size;
+
+        public SerializableClass(String title, int size) {
+            this.title = title;
+            this.size = size;
         }
     }
 }
