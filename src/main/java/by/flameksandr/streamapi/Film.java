38ce5bd8f94efa61e9a90a7e193861ee026d52ec
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 16:15:04 +0300

    преобразование элементов

---
+++ src/main/java/by/flameksandr/streamapi/Film.java
@@ -0,0 +1,11 @@
+package by.flameksandr.streamapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Film {
+    private String title;
+    private double price;
+}
--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -5,8 +5,8 @@ import java.util.stream.Stream;
 public class Starter {
     public static void main(String[] args) {
 
-        Stream<String> stream = Stream.of("val1", "val2", "val3", "val4", "val5");
-//        stream.skip(2).forEach(System.out::println);
-        stream.limit(3).forEach(System.out::println);
+        Stream<Film> filmStream = Stream.of(new Film("DeadPool", 100.0), new Film("Carbiners", 200.0), new Film("Mortal Combat", 150.0));
+        filmStream.map(Film::getPrice).forEach(System.out::println);
+
     }
 }
