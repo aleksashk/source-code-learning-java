8250ecfb4422d6439ad0559d83d592c777bacb62
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-25 18:32:01 +0300

    Группировка данных V2

--- src/main/java/by/flameksandr/streamapi/Starter.java
+++ src/main/java/by/flameksandr/streamapi/Starter.java
@@ -15,22 +15,13 @@ public class Starter {
                 new Computer("Laptop", "ACER", "Lamba", 5688),
                 new Computer("Laptop", "MARSEL", "ROMB", 7840)
         );
-        Map<Boolean, List<Computer>> computers = stream.collect(Collectors.partitioningBy(c -> c.getPrice() > 1000));
-        for (Map.Entry<Boolean, List<Computer>> item : computers.entrySet()) {
-            if (item.getKey()) {
-                showProducts("more", item);
-            } else {
-                showProducts("less", item);
+        Map<String, List<Computer>> computers = stream.collect(Collectors.groupingBy(Computer::getType));
+        for (Map.Entry<String, List<Computer>> item : computers.entrySet()) {
+            System.out.println("Type: " + item.getKey());
+            for (Computer c : item.getValue()) {
+                System.out.printf("Company: %s, Model: %s, Price: %d %n", c.getCompany(), c.getModel(), c.getPrice());
             }
+            System.out.println();
         }
-
-    }
-
-    private static void showProducts(String status, Map.Entry<Boolean, List<Computer>> item) {
-        System.out.printf("Price is %s than $1000: %n", status);
-        for (Computer c : item.getValue()) {
-            System.out.printf("Type: %s, Company: %s, Model: %s %n", c.getType(), c.getCompany(), c.getModel());
-        }
-        System.out.println();
     }
 }
